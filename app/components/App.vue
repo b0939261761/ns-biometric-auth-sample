062ffd483f7cf2eaae5ab2f78b9533b87299a12f
b0939261761 <b0939261761@gmail.com>
2019-09-24 22:11:14 +0300

    first commit

---
+++ app/components/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <Page>
+    <ActionBar title="nativescript biometric auth sample"/>
+    <StackLayout orientation="vertical">
+      <Button @tap = 'onClick' text = 'open' />
+      <Label class="message" :text="status"/>
+    </StackLayout>
+  </Page>
+</template>
+
+<script>
+import { checkFingerprint, openFingerprint } from '@/fingerprint';
+
+export default {
+  data: () => ({
+    status: ''
+  }),
+  mounted() {
+    this.status = checkFingerprint();
+  },
+  methods: {
+    onClick() {
+      openFingerprint();
+    }
+  }
+}
+</script>
+
